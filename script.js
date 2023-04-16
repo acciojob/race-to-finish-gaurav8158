c65e2f1a48159a2af8f6983cfb1e40c537f06281
Gaurav Soni <120136122+gaurav8158@users.noreply.github.com>
2023-04-17 00:14:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 window.promises = [];
-const p1 = new Promise(resolve()=>setTimeout(resolve,1000));
-const p2 = new Promise(resolve()=>setTimeout(resolve,1000));
-const p3 = new Promise(resolve()=>setTimeout(resolve,1000));
-const p4 = new Promise(resolve()=>setTimeout(resolve,1000));
-const p5 = new Promise(resolve()=>setTimeout(resolve,1000));
-
-let arr = [p1,p2,p3,p4,p5];
-const div = document.getElementById("output");
-Promise.any(promises).then((data) => 
-	div.value+=data;
-	// console.log(value)
-						  );
+let promises = [
+  new Promise((resolve) => setTimeout(() => resolve(Math.random() * 5 + 1), Math.random() * 5 + 1)),
+  new Promise((resolve) => setTimeout(() => resolve(Math.random() * 5 + 1), Math.random() * 5 + 1)),
+  new Promise((resolve) => setTimeout(() => resolve(Math.random() * 5 + 1), Math.random() * 5 + 1)),
+  new Promise((resolve) => setTimeout(() => resolve(Math.random() * 5 + 1), Math.random() * 5 + 1)),
+  new Promise((resolve) => setTimeout(() => resolve(Math.random() * 5 + 1), Math.random() * 5 + 1))
+];
+Promise.any(promises).then(result => {
+  document.getElementById('output').innerHTML = result;
+});
 // Do not change the code above this
 // add your promises to the array `promises`

7bade3b64e0bbca23abc7747857a9cd92cae3c5c
Gaurav Soni <120136122+gaurav8158@users.noreply.github.com>
2023-04-16 23:58:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 window.promises = [];
+const p1 = new Promise(resolve()=>setTimeout(resolve,1000));
+const p2 = new Promise(resolve()=>setTimeout(resolve,1000));
+const p3 = new Promise(resolve()=>setTimeout(resolve,1000));
+const p4 = new Promise(resolve()=>setTimeout(resolve,1000));
+const p5 = new Promise(resolve()=>setTimeout(resolve,1000));
 
+let arr = [p1,p2,p3,p4,p5];
+const div = document.getElementById("output");
+Promise.any(promises).then((data) => 
+	div.value+=data;
+	// console.log(value)
+						  );
 // Do not change the code above this
 // add your promises to the array `promises`
